239b71f6bec7c52dbab87618b6ce6041e5eb735a
bdbrink <brendan.brink@outlook.com>
2023-09-20 16:02:56 -0500

    adding OS from user

--- main.go
+++ main.go
@@ -10,18 +10,22 @@ func getCurrentTime(w http.ResponseWriter, r *http.Request) {
 	// Get the IP address of the requester
 	ipAddress := r.RemoteAddr
 
+	// Get the User-Agent header from the request
+	userAgent := r.Header.Get("User-Agent")
+
 	// Get the current UTC time
 	currentTime := time.Now().UTC()
 
 	// Format the time as a string
 	timeString := currentTime.Format("2006-01-02T15:04:05.999Z")
 
-	// Log the IP address and time
+	// Log the IP address and User-Agent
 	fmt.Printf("Request from IP: %s\n", ipAddress)
+	fmt.Printf("User-Agent: %s\n", userAgent)
 
-	// Return the time as a JSON response
+	// Return the time and User-Agent as a JSON response
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, `{"utc_time": "%s"}`, timeString)
+	fmt.Fprintf(w, `{"utc_time": "%s", "user_agent": "%s"}`, timeString, userAgent)
 }
 
 func main() {
