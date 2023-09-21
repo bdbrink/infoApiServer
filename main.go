44fc7dd8ce948edcbd08485cdea9cccf1e3ee8cc
bdbrink <brendan.brink@outlook.com>
2023-09-21 17:09:57 -0500

    adding location to request

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 )
 
-func getCurrentTime(w http.ResponseWriter, r *http.Request) {
+type LocationResponse struct {
+	IP        string  `json:"ip"`
+	City      string  `json:"city"`
+	Region    string  `json:"region"`
+	Country   string  `json:"country"`
+	Latitude  float64 `json:"loc"`
+	Longitude float64 `json:"loc"`
+}
+
+func getCurrentTimeAndLocation(w http.ResponseWriter, r *http.Request) {
 	// Get the IP address of the requester
 	ipAddress := r.RemoteAddr
 
@@ -15,22 +25,37 @@ func getCurrentTime(w http.ResponseWriter, r *http.Request) {
 
 	// Get the current UTC time
 	currentTime := time.Now().UTC()
-
-	// Format the time as a string
 	timeString := currentTime.Format("2006-01-02T15:04:05.999Z")
 
-	// Log the IP address and User-Agent
+	// Make a request to ipinfo.io to get the location information
+	locationURL := "https://ipinfo.io/" + ipAddress + "/json"
+	resp, err := http.Get(locationURL)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Decode the response JSON into a LocationResponse struct
+	var location LocationResponse
+	if err := json.NewDecoder(resp.Body).Decode(&location); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Log the IP address, User-Agent, and location
 	fmt.Printf("Request from IP: %s\n", ipAddress)
 	fmt.Printf("User-Agent: %s\n", userAgent)
+	fmt.Printf("Location: %s, %s, %s\n", location.City, location.Region, location.Country)
 
-	// Return the time and User-Agent as a JSON response
+	// Return the time, User-Agent, and location as a JSON response
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, `{"utc_time": "%s", "user_agent": "%s"}`, timeString, userAgent)
+	fmt.Fprintf(w, `{"utc_time": "%s", "user_agent": "%s", "location": "%s, %s, %s"}`, timeString, userAgent, location.City, location.Region, location.Country)
 }
 
 func main() {
-	// Define a route for getting the current UTC time
-	http.HandleFunc("/current-time", getCurrentTime)
+	// Define a route for getting the current UTC time and location
+	http.HandleFunc("/current-time-and-location", getCurrentTimeAndLocation)
 
 	// Start the server
 	port := 8080
